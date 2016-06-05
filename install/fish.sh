9e6f28f6a36f9d7811dd90505fe5173e9f9456b1
Marnu <marnu@lombard>
2016-06-05 22:00:07 +0200

    Install fish before attempting to use it...

--- install/fish.sh
+++ install/fish.sh
@@ -10,6 +10,25 @@ echo "--------------------------------------
     Starting Fish Shell installation
 --------------------------------------"
 
+if [ X"${DISTRO}" == X'DARWIN' ]; then
+  brew install fish
+
+elif [ X"${DISTRO}" == X'UBUNTU' ]; then
+  sudo apt-add-repository ppa:fish-shell/release-2
+  sudo apt-get update
+  sudo apt-get install -y fish
+
+elif [ X"${DISTRO}" == X'FEDORA' ]; then
+  # Simple trick to parse OS version details
+  # See: https://github.com/ValveSoftware/steam-for-linux/issues/2005#issuecomment-14521331
+  . /etc/os-release
+
+  curl "http://download.opensuse.org/repositories/shells:fish:release:2/Fedora_$VERSION_ID/shells:fish:release:2.repo" -o /etc/yum.repos.d/fish.repo
+  sudo dnf install -y fish
+
+fi
+
+
 # Install Oh My Fish
 curl -L https://github.com/oh-my-fish/oh-my-fish/raw/master/bin/install | fish
 
